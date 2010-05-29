1882b01d48f31b881774375db0cdccb4bee6a9d1
petersen.arne <petersen.arne@d7f895ec-9d1c-11dd-93bb-3f8bb607bf45>
2010-05-29 16:43:08 +0000

    Gedanken zum Spiel aufgeschrieben

---
+++ tichu/src/net/wsgs/process/Tichu.java
@@ -0,0 +1,39 @@
+package net.wsgs.process;
+
+public class Tichu {
+
+	/**
+	 * Nur so Gedanken zum Spiel
+	 */
+	public Tichu() {
+		// Spiel anlegen
+
+		// Alle Spiele (4) definieren
+
+		// Team bilden
+
+		// Lasse die Spiele beginnen!
+		// Karten geben
+
+		// Karten schupfen
+
+		// Spieler mit dem Mahjongg (Die 1) fängt an und darf sich eine Karte
+		// wünschen
+
+		// Der Spieler beginnt die Runde und legt eine Kartenkombination (oder
+		// eine Einzelkarte)
+
+		/*
+		 * [SCHLEIFE] existiert noch ein offener Wunsch? so muss er wenn möglich
+		 * erfüllt werden. Anderen falls darf er einen passenden Zug machen,
+		 * dazu gehört auch das Passen.
+		 * 
+		 * [ENDE] Wenn drei Spieler infolge Passen.
+		 */
+		
+		// Punkte werden gezählt und dem Team des Spielers zugewiesen der nicht gepasst hat.
+		
+		// Dieser Spieler beginnt die nächste Runde.
+	}
+
+}
