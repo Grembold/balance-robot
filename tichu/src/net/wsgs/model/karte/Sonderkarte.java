01958ac9f7bb9121e458021c1282a7b18c416388
petersen.arne <petersen.arne@d7f895ec-9d1c-11dd-93bb-3f8bb607bf45>
2010-05-30 17:50:17 +0000

    Sonderkarten um die definition der werte für diese karten erweitert

--- tichu/src/net/wsgs/model/karte/Sonderkarte.java
+++ tichu/src/net/wsgs/model/karte/Sonderkarte.java
@@ -3,10 +3,19 @@ package net.wsgs.model.karte;
 /**
  * Diese Klasse erweitert eine normale Karte zu einer Sonderkarte
  * 
+ * Sonderkarten:<br>
+ * <li>Der Hund hat den Wert 0</li> <li>Der Mahjongg hat den Wert 1</li> <li>Der
+ * Phönix hat den Wert 15</li> <li>Der Drache hat den Wert 16</li>
+ * 
  * @author Arne
  * 
  */
 public class Sonderkarte extends Karte {
+
+	public static final int HUND_WERT = 0;
+	public static final int MAHJONGG_WERT = 1;
+	public static final int PHOENIX_WERT = 15;
+	public static final int DRACHEN_WERT = 16;
 
 	private SonderkartenTyp typ;
 
